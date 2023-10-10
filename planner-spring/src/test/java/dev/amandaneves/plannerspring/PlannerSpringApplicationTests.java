5cf616df298f2720fe61ec64f25239fc70eb6eb8
Amanda Neves <amandaneves.carmo@gmail.com>
2023-10-09 21:03:59 -0300

    Adicionando Planner Spring

---
+++ planner-spring/src/test/java/dev/amandaneves/plannerspring/PlannerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.amandaneves.plannerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

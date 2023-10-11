de8ffd1fd4a0e2395715bfb574b53c6992026dd9
Amanda Neves <amandaneves.carmo@gmail.com>
2023-10-11 00:53:10 -0300

    Ajustes para fazer deploy do backend para AWS Beanstalk

--- planner-spring/src/main/java/dev/amandaneves/plannerspring/PlannerSpringApplication.java
+++ planner-spring/src/main/java/dev/amandaneves/plannerspring/PlannerSpringApplication.java
@@ -2,12 +2,21 @@ package dev.amandaneves.plannerspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping(value = "/api")
 public class PlannerSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlannerSpringApplication.class, args);
 	}
 
+	@GetMapping
+	public String helloWorld() {
+		return "Hello World!";
+	}
 }
